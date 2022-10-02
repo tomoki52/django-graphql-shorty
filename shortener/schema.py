9d3559f9404e263bf6b819156b4ba6ef512d5f53
tomoki52 <konishi.tomoki.ap@alumni.tsukuba.ac.jp>
2022-10-02 13:30:55 +0900

    mutationの追加

--- shortener/schema.py
+++ shortener/schema.py
@@ -15,3 +15,21 @@ class Query(graphene.ObjectType):
 
     def resolve_urls(self, info, **kwargs):
         return URL.objects.all()
+
+
+class CreateURL(graphene.Mutation):
+    # Mutation後にサーバから返されるデータ
+    url = graphene.Field(URLType)
+
+    # サーバが受け入れるデータ
+    class Arguments:
+        full_url = graphene.String()
+
+    def mutate(self, info, full_url):
+        url = URL(full_url=full_url)
+        url.save()
+        return CreateURL(url=url)
+
+
+class Mutation(graphene.ObjectType):
+    create_url = CreateURL.Field()
--- shorty/schema.py
+++ shorty/schema.py
@@ -6,4 +6,8 @@ class Query(shortener.schema.Query, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(shortener.schema.Mutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
